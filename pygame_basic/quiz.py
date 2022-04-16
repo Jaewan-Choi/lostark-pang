d43cf1c3106fa1ac0ccb39b5e4b148aa30c0df55
JWCHOI <wkdudhksl@naver.com>
2022-04-17 01:34:20 +0900

    게임 완성

---
+++ pygame_basic/quiz.py
@@ -0,0 +1,106 @@
+import random
+import pygame
+###############################################################################
+# 기본 초기화 (반드시 해야 하는 것들)
+pygame.init() # 초기화 (반드시 필요)
+
+# 화면 크기 설정
+screen_width = 480 # 가로 크기
+screen_height = 630 # 세로 크기
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# 화면 타이틀 설정
+pygame.display.set_caption("Quiz") # 게임 이름
+
+# FPS
+clock = pygame.time.Clock()
+###############################################################################
+
+# 1. 사용자 게임 초기화 (배경 화면, 게임 이미지, 좌표, 속도, 폰트 등)
+
+# 배경 만들기
+background = pygame.image.load("C:/Users/최재완/Desktop/PythonWorkspace/pygame_basic/background.png")
+
+# 캐릭터 만들기
+character = pygame.image.load("C:/Users/최재완/Desktop/PythonWorkspace/pygame_basic/character.png")
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = (screen_width / 2) - (character_width / 2)
+character_y_pos = screen_height - character_height
+
+# 적 만들기
+enemy = pygame.image.load("C:/Users/최재완/Desktop/PythonWorkspace/pygame_basic/enemy.png")
+enemy_size = enemy.get_rect().size
+enemy_width = enemy_size[0]
+enemy_height = enemy_size[1]
+enemy_x_pos = random.randint(0, screen_width - enemy_width)
+enemy_y_pos = 0
+enemy_speed = 10
+
+# 이동 위치
+to_x = 0
+to_y = 0
+
+character_speed = 0.6 # 게임 속도
+
+running = True
+while running:
+    dt = clock.tick(30)
+
+    # 2. 이벤트 처리 (키보드, 마우스 등)
+    for event in pygame.event.get(): # 어떤 이벤트가 발생하였는가?
+        if event.type == pygame.QUIT: # 창이 닫히는 이벤트가 발생하였는가?
+            running = False # 게임이 진행중이 아님
+
+        # 캐릭터 이동하기
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                to_x -= character_speed
+            elif event.key == pygame.K_RIGHT:
+                to_x += character_speed
+
+        # 캐릭터 이동 멈추기
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+
+    # 3. 게임 캐릭터 위치 정의
+    character_x_pos += to_x * dt
+    character_y_pos += to_y * dt
+
+    # 화면 바깥으로 나가지 않도록함 (가로)
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    # 적 위치 이동
+    enemy_y_pos += enemy_speed
+
+    if enemy_y_pos > screen_height:
+        enemy_y_pos = 0
+        enemy_x_pos = random.randint(0, screen_width - enemy_width)
+
+    # 4. 충돌 처리
+    character_rect = character.get_rect()
+    character_rect.left = character_x_pos
+    character_rect.top = character_y_pos
+
+    enemy_rect = enemy.get_rect()
+    enemy_rect.left = enemy_x_pos
+    enemy_rect.top = enemy_y_pos
+
+    if character_rect.colliderect(enemy_rect):
+        print("충돌")
+        running = False
+
+    # 5. 화면에 그리기
+    screen.blit(background, (0, 0))
+    screen.blit(character, (character_x_pos, character_y_pos))
+    screen.blit(enemy, (enemy_x_pos, enemy_y_pos)) # x 좌표 랜덤 필요함
+
+    pygame.display.update() # 게임화면을 다시 그리기!
+
+# pygame 종료
+pygame.quit()
